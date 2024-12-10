08f5c81e1b29babb5505c1c5cf492422594de4f3
bosung si <qweasdzxc0311@naver.com>
2024-12-10 20:16:37 +0900

    counter 추가 기능

--- src/counter/ConterBox.tsx
+++ src/counter/ConterBox.tsx
@@ -4,6 +4,7 @@ import styled from './CounterBox.module.css'
 
 export default function CounterBox() {
     const [total, setTotal] = useState<number>(0);
+    const [counter, setCounter] = useState<JSX.Element[]>([]);
     
     const handlePlus = () => {
         setTotal(prev => prev + 1);
@@ -12,10 +13,21 @@ export default function CounterBox() {
     const handleMinus = () => {
         setTotal(prev => prev - 1);
     }
+
+    const handleAddCount = () => {
+        setCounter(prevCounters => [
+            ...prevCounters,
+            <Counter key={prevCounters.length} onMinus={handleMinus} onPlus={handlePlus}></Counter>
+        ])
+    }
+
+
     
     return (
         <>
+            <button onClick={handleAddCount}>카운터 추가하기</button>
             <div id={styled.counterBox}>
+                {counter}
                 <Counter onMinus={handleMinus} onPlus={handlePlus}></Counter>
                 <Counter onMinus={handleMinus} onPlus={handlePlus}></Counter>
             </div>
